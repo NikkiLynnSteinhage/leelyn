f00d43952b78bda8fcb504c8f00cae2938823a08
NikkiLynnSteinhage <nikkij@leelyn.co.za>
2026-03-19 00:55:57 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,13 @@
-import { ContactPage } from "@/components/site-shell";
+import Script from "next/script";
 
-export default function Page() {
-  return <ContactPage />;
-}
+<div className="mt-6">
+  <div
+    style={{ width: "100%", height: "500px" }}
+    data-fillout-id="c3wvpBNZ6dus"
+    data-fillout-embed-type="standard"
+    data-fillout-inherit-parameters
+    data-fillout-dynamic-resize
+  ></div>
+
+  <Script src="https://server.fillout.com/embed/v1/" strategy="lazyOnload" />
+</div>
